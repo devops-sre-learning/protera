7c00732578d22d4dc6532686e0abcc2d5c2d4105
devops-sre-learning <ryan.lukasik@gmail.com>
2025-10-12 23:12:03 -0500

    added terraform version to readme, added providers, main, vars files to start.  Added google to providers

---
+++ question_3/README.md
@@ -0,0 +1,3 @@
+Terraform v1.13.3 on linux_amd64
+
+
---
+++ question_3/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.6.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+
+}
